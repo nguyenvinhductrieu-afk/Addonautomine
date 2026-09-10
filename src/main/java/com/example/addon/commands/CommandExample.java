7a202bd93d79100772459bf5df39b9222efb139d
nguyenvinhductrieu-afk <nguyenvinhductrieu@gmail.com>
2026-09-10 16:29:14 +0700

    Update CommandExample.java

--- src/main/java/com/example/addon/commands/CommandExample.java
+++ src/main/java/com/example/addon/commands/CommandExample.java
@@ -18,7 +18,7 @@ public class CommandExample extends Command {
 
     @Override
     public void build(LiteralArgumentBuilder<ClientSuggestionProvider> builder) {
-        builder.executes(_ -> {
+        builder.executes(content -> {
             info("hi");
             return SINGLE_SUCCESS;
         });
