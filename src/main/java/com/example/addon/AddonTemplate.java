6452426d1d35aec4387fdb91a92f4ece39b691ac
nguyenvinhductrieu-afk <nguyenvinhductrieu@gmail.com>
2026-09-10 16:41:33 +0700

    Remove duplicate import of Category

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -12,6 +12,7 @@ import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
+import meteordevelopment.meteorclient.systems.modules.Category;
 
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
